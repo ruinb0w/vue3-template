78cd7524bd3bb0270de3d1463924f770b9cd7e43
Hans <ruinb0w@qq.com>
2025-05-19 18:58:19 +0800

    add analyze

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,24 @@ import vue from '@vitejs/plugin-vue'
 import path from "node:path";
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver, ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({
-    resolvers: [VantResolver(), ElementPlusResolver()],
-  })],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver(), ElementPlusResolver(),],
+    }),
+    process.env.VISUALIZE === 'true' && visualizer({
+      open: true,
+      filename: 'stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    })
+  ],
   base: './',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
